083eb93c0f1f8c384a379976a178848bfbbcce04
Pat Erley <pat-lkml@erley.org>
2012-06-12 13:37:53 -0400

    Port stepper.pl to UBW32.pm
    This removes all direct dependence on talking to the UBW32 out of
    stepper.pl.

--- stepper.pl
+++ stepper.pl
@@ -1,10 +1,11 @@
 #!/bin/perl
 
 use strict;
-use Device::SerialPort;
 use Time::HiRes qw(usleep nanosleep);
-
+use UBW32 qw(:DEFAULT);
 my $serialport = "/dev/ttyACM0";
+
+my $ubw=UBW32->new($serialport);
 
 my %motors = (
    pan => {
@@ -58,98 +59,14 @@ my %motors = (
 # Abandon all hope, ye who enter here
 #
 
-my $dbg = 0;
-
-my $port;
-my %pinconfigs;
-
-sub validate_pin {
-   my $group = shift;
-   my $pin = shift;
-   if( $group !~ /[ABCDEFG]/ ) {
-     printf("Skipping pin %s%s: Invalid pingroup passed [%s] valid: A-G\n", $group, $pin, $group);
-     return 1;
-   } elsif( $pin > 15 || $pin < 0 ) {
-     printf("Skipping pin %s%S: Invalid pin [%s] valid: 0-15\n", $group, $pin, $pin);
-     return 1;
-   }
-   return 0;
-}
-
-sub configure_pin {
-  my $group = shift;
-  my $pin = shift;
-  my $dir = shift;
-  my $result = "";
-  my $dir_bit = ($dir eq "in") ? 1 : 
-                ($dir eq "out") ? 0 :
-		-1;
-  my $cmd = sprintf("PD,%s,%s,%s" , $group,$pin,$dir_bit);
-  if($dir_bit < 0) {
-     printf("Skipping pin %s%s: Invalid direction was passed [%s] valid: {in,out}\n", $group, $pin, $dir);
-  } elsif( validate_pin($group,$pin) ){  
-  } else {
-     if( !$dbg ) {
-       $port->write("$cmd\n");
-       $port->write_drain;
-       usleep(10*1000);
-       $result = $port->read(255);
-     } else {
-       $result = "OK";
-     }
-     if( $result !~ /OK/ ) {
-	printf("Pin configuration failed: [%s][$cmd]\n", $result);
-     } else {
-       $pinconfigs{"$group$pin"}{direction} = $dir;
-       $pinconfigs{"$group$pin"}{state} = "Unknown";
-     }
-  }
-}
-
-sub set_pin {
-  my $group = shift;
-  my $pin = shift;
-  my $value = shift;
-  my $result = "";
-  my $val_bit = ($value eq "low") ? 0 :
-                ($value eq "high") ? 1 :
-		-1;
-
-  my $cmd = sprintf("PO,%s,%s,%s", $group,$pin,$val_bit);
-  if( $val_bit < 0 ) {
-     printf("Skipping pin %s%s: Invalid pin state was passed [%s] valid: {high,low}\n", $group, $pin, $value);
-  } elsif( validate_pin($group,$pin) ){
-  } elsif( $pinconfigs{"$group$pin"}{direction} ne "out" ) {
-     printf("Skipping pin %s%s: Pin is set to input or no direction explicitly set\n", $group, $pin);
-  } else {
-    if(!$dbg){
-      $port->write("$cmd\n");
-      $port->write_drain;
-      usleep(7.5*1000);
-      $result = $port->read(255);
-    } else {
-      $result = "OK";
-    }
-    if( $result !~ /OK/ ) {
-       printf("Pin state failed: [%s]\n", $result);
-    } else {
-       $pinconfigs{"$group$pin"}{state} = $value;
-    }
-  }
-}
-
-sub print_pin {
-  my @cfg = @{$_[0]};
-  return "${cfg[0]}${cfg[1]}";
-}
 
 sub list_motors {
   my $name;
   foreach $name (keys(%motors)) {
     printf("Motor: %s [ Step: %s, Direction: %s ]\n",
            $name, 
-	   print_pin(\@{$motors{$name}{step}}),
-	   print_pin(\@{$motors{$name}{direction}})
+	   $ubw->print_pin(\@{$motors{$name}{step}}),
+	   $ubw->print_pin(\@{$motors{$name}{direction}})
     );
   }
 }
@@ -166,9 +83,9 @@ sub configure_motor_pins {
       my $value = $motors{$motor}{$function}[3];
 
       printf("Configuring: %s -> %s\n", $motor, $function);
-      configure_pin($group, $pin, $dir);
+      $ubw->configure_pin($group, $pin, $dir);
       if($dir eq "out") {
-        set_pin($group, $pin, $value);
+        $ubw->set_pin($group, $pin, $value);
       }
     }
   }
@@ -188,21 +105,21 @@ sub step_motor {
   my $dir_bit = $motors{$motor}{$direction};
   my $steps = $angle / $motors{$motor}{stepsize};
   
-  set_pin($dircfg[0], $dircfg[1], $dir_bit);
+  $ubw->set_pin($dircfg[0], $dircfg[1], $dir_bit);
 
   if($motors{$motor}{canustep}){
     if($ustepping) {
       my @ustepcfg = @{$motors{$motor}{usteps}{$ustepping}};
-      set_pin($ms1cfg[0], $ms1cfg[1], $ustepcfg[0]);
-      set_pin($ms2cfg[0], $ms2cfg[1], $ustepcfg[1]);
+      $ubw->set_pin($ms1cfg[0], $ms1cfg[1], $ustepcfg[0]);
+      $ubw->set_pin($ms2cfg[0], $ms2cfg[1], $ustepcfg[1]);
       $steps /= $ustepcfg[2];
     }
   }
 
   for(; $steps > 0; $steps--) {
-    set_pin($stepcfg[0], $stepcfg[1], "high");
+    $ubw->set_pin($stepcfg[0], $stepcfg[1], "high");
     usleep($motors{$motor}{holdtime});
-    set_pin($stepcfg[0], $stepcfg[1], "low");
+    $ubw->set_pin($stepcfg[0], $stepcfg[1], "low");
   }
 }
 
@@ -211,7 +128,7 @@ sub sleep_motor {
   my @sleepcfg = @{$motors{$motor}{slp}};
   my $sleepval = $motors{$motor}{sleepval};
   
-  set_pin($sleepcfg[0], $sleepcfg[1], $sleepval);
+  $ubw->set_pin($sleepcfg[0], $sleepcfg[1], $sleepval);
 }
 
 sub wake_motor {
@@ -219,15 +136,7 @@ sub wake_motor {
   my @sleepcfg = @{$motors{$motor}{slp}};
   my $sleepval = $motors{$motor}{sleepval};
 
-  set_pin($sleepcfg[0], $sleepcfg[1], $sleepval eq "high" ? "low" : "high");
-}
-
-if(!$dbg) {
-  $port = Device::SerialPort->new("/dev/ttyACM0");
-  $port->databits(8);
-  $port->baudrate(9600);
-  $port->parity("none");
-  $port->stopbits(1);
+  $ubw->set_pin($sleepcfg[0], $sleepcfg[1], $sleepval eq "high" ? "low" : "high");
 }
 
 #testing
